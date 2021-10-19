26220a7fc87ed63d999854d57624d0d50a1e58bb
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-10-19 13:46:00 -0300

    pack jason

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-/* module.exports = () => {
-  // ...
-}; */
-
 const chalk = require('chalk');
 const fs = require('fs'); // lib que acessa arquivos
+const path = require('path');
 
 function pegaArquivo (caminhoArquivo) {
   const encoding = 'utf-8'
   fs.readFile(caminhoArquivo, encoding, (_, texto) => {
-    console.log(chalk.green(texto));
+    console.log(chalk.yellow(texto));
   })
 }
 
 pegaArquivo('./README.md');
 
 /*  
+regex dentro de uma const
+se diretório - promise para ver se é arquivo md ou não
+vi q a diferença deles é q o exec busca string e retorna o array, o match retorna uma string com regex 
  console.log(chalk.blue("bin")) 
+
+
+ 
 ///fs.readFile(file, [encoding], [callback]); 
 function trataErro(erro) { throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho')); } 
 function pegaArquivo (caminhoArquivo) { const encoding = 'utf-8' fs.readFile(caminhoArquivo, encoding, (erro, texto) => { if (erro) { trataErro(erro); } console.log(chalk.green(texto)) }) } 
