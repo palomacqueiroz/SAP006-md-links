12667acfc7a219fc1f48734bc08a7a3656518112
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-10-25 22:13:31 -0300

    funçao pegar arquivo - promise

--- index.js
+++ index.js
@@ -2,34 +2,47 @@ const chalk = require('chalk');
 const fs = require('fs'); // lib que acessa arquivos
 const path = require('path');
 
-function pegaArquivo (caminhoArquivo) {
-  const encoding = 'utf-8'
-  fs.readFile(caminhoArquivo, encoding, (_, texto) => {
-    console.log(chalk.yellow(texto));
-  })
+function tratarErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Verificar o caminho do arquivo'));
 }
 
-pegaArquivo('./README.md');
+async function getFile (pathFile) {
+  const encoding = 'utf-8'
+  try{
+  const text = await fs.promises.readFile(pathFile, encoding)
+  console.log(chalk.yellow(text)) // recebe conteúdo do arquivo - sempre passar por parametro
+  } catch(erro){
+    tratarErro(erro)
+  }
+  }
+
+  getFile('./README.md');
+
+/* function getFile (pathFile) {
+  const encoding = 'utf-8'
+  fs.promises.readFile(pathFile, encoding)
+  .then((text) => console.log(chalk.yellow(text))) // recebe conteúdo do arquivo - sempre passar por parametro
+  .catch((erro) => tratarErro(erro))
+} */
+
+/* function pegaArquivo (caminhoArquivo) {
+  const encoding = 'utf-8'
+  fs.readFile(caminhoArquivo, encoding, (erro, texto) => {
+    if(erro) {
+      tratarErro(erro);
+    }
+    console.log(chalk.yellow(texto));
+  })
+} */
 
 /*  
 regex dentro de uma const
 se diretório - promise para ver se é arquivo md ou não
 vi q a diferença deles é q o exec busca string e retorna o array, o match retorna uma string com regex 
  console.log(chalk.blue("bin")) 
-
-
- 
 ///fs.readFile(file, [encoding], [callback]); 
-function trataErro(erro) { throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho')); } 
-function pegaArquivo (caminhoArquivo) { const encoding = 'utf-8' fs.readFile(caminhoArquivo, encoding, (erro, texto) => { if (erro) { trataErro(erro); } console.log(chalk.green(texto)) }) } 
-pegaArquivo("./SAP006-md-links/arquivos/teste.md") 
-
-console.log(chalk.blue("que que há velhinho"));
-
-const paragrafo = 'Testo retornado por uma função'
 
 function texto(string) {
     return string;
 }
-
-console.log(texto(paragrafo)); */
+*/
