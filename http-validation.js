eee968bbe987dd35edf843a5c2182039ffdd5e49
Paloma Queiroz <palomacqueiroz@hotmail.com>
2021-10-28 13:25:57 -0300

    pre pull

--- http-validation.js
+++ http-validation.js
@@ -1,5 +1,24 @@
-function validaUrls(arrayLinks) {
-    return arrayLinks;
+const fetch = require('node-fetch');
+
+
+async function checkStatus(arrURLs) {
+    const arrStatus = await Promise.all(arrURLs.map(async url => {
+        const res = await fetch(url)
+        return res.status;
+    }))
+    return arrStatus;
+}
+
+function generateArrLinks(arrayLinks){
+    //fazer loop para iterar cada objeto
+    // Object.values(objeto)  // retorna valor de cada objto(chave:valor)
+    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());  // join tita do array e transforma em string
+}
+
+async function validaUrls(arrayLinks) {
+    const links = generateArrLinks(arrayLinks);
+    const statusLinks = await checkStatus(links);
+    return statusLinks;
 }
 
 module.exports = validaUrls;
